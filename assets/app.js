c70c91b5722fd17e15c8f6951d9fa7e99769b619
Your Name <bloodsportrecords@gmail.com>
2022-08-27 12:26:43 +0100

    add function and functional variables for scroll text on hero content

--- assets/app.js
+++ assets/app.js
@@ -2,5 +2,9 @@
 const headerText = document.getElementById("header-text");
 
 // Functions
-
+function scrollOptions() {
+    const rightArrow = document.getElementById("right-arrow");
+    const leftArrow = document.getElementById("left-arrow");
+};
 // Event Listeners
+
