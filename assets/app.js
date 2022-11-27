d15585e08720774f8986e6edb0b19810ee96ddb5
Your Name <bloodsportrecords@gmail.com>
2022-11-27 20:35:37 +0000

    add form validation

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,8 @@
 function validateForm() {
-  let x = document.forms["myForm"]["fname"].value;
-  if (x == "") {
-    alert("Name must be filled out");
+  let fName= document.forms["myForm"]["fname"].value;
+  let email= document.forms["myForm"]["email"].value;
+  if (fName == "" || email == "") {
+    alert("Name and email cannot be empty");
     return false;
   }
 }
